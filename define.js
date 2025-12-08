b8eeaaa6fb5e317055ed9aeb46e080464635cc84
Aaron Gustafson <aaron@easy-designs.net>
2025-12-07 20:53:38 -0800

    Harden definition

--- define.js
+++ define.js
@@ -1,8 +1,20 @@
 import { FormRequiredCheckboxesElement } from './form-required-checkboxes.js';
 
-if (!!customElements) {
-	customElements.define(
-		'form-required-checkboxes',
-		FormRequiredCheckboxesElement,
-	);
+export function defineFormRequiredCheckboxes(
+	tagName = 'form-required-checkboxes',
+) {
+	const hasWindow = typeof window !== 'undefined';
+	const registry = hasWindow ? window.customElements : undefined;
+
+	if (!registry || typeof registry.define !== 'function') {
+		return false;
+	}
+
+	if (!registry.get(tagName)) {
+		registry.define(tagName, FormRequiredCheckboxesElement);
+	}
+
+	return true;
 }
+
+defineFormRequiredCheckboxes();
