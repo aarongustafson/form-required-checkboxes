f712052abd2023634112e5e6f014ec1ad94c8858
Aaron Gustafson <aaron@easy-designs.net>
2025-12-13 09:36:36 -0800

    Add type definitions

---
+++ form-required-checkboxes.d.ts
@@ -0,0 +1,28 @@
+// Type definitions for form-required-checkboxes web component
+// Project: form-required-checkboxes
+// Definitions by: Aaron Gustafson
+
+export class FormRequiredCheckboxesElement extends HTMLElement {
+  /**
+   * The notice message for the group.
+   */
+  notice: string | null;
+  /**
+   * The error message for the group.
+   */
+  error: string | null;
+  /**
+   * The required number or range of checkboxes.
+   */
+  required: string | null;
+  /**
+   * The language code for translations.
+   */
+  lang: string | null;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'form-required-checkboxes': FormRequiredCheckboxesElement;
+  }
+}
