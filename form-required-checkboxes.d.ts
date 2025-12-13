e0d99c560a2cfab725aa13c5d457a78d50cef8ec
Aaron Gustafson <aaron@easy-designs.net>
2025-12-13 14:22:18 -0800

    lang cannot be null

--- form-required-checkboxes.d.ts
+++ form-required-checkboxes.d.ts
@@ -3,26 +3,26 @@
 // Definitions by: Aaron Gustafson
 
 export class FormRequiredCheckboxesElement extends HTMLElement {
-  /**
-   * The notice message for the group.
-   */
-  notice: string | null;
-  /**
-   * The error message for the group.
-   */
-  error: string | null;
-  /**
-   * The required number or range of checkboxes.
-   */
-  required: string | null;
-  /**
-   * The language code for translations.
-   */
-  lang: string | null;
+	/**
+	 * The notice message for the group.
+	 */
+	notice: string | null;
+	/**
+	 * The error message for the group.
+	 */
+	error: string | null;
+	/**
+	 * The required number or range of checkboxes.
+	 */
+	required: string | null;
+	/**
+	 * The language code for translations.
+	 */
+	lang: string;
 }
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'form-required-checkboxes': FormRequiredCheckboxesElement;
-  }
+	interface HTMLElementTagNameMap {
+		'form-required-checkboxes': FormRequiredCheckboxesElement;
+	}
 }
